561c5aca63e23fbc53bfd3f2cf906e7c60e42e8c
Svetlana <brigantina@mail.ru>
2025-09-16 19:55:29 +0300

    feat: solution task 2

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -40,8 +40,16 @@ function getStringLength(value) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  // проверка приметив
+  if (typeof value === 'string') {
+    return true;
+  }
+  // проверка объекта
+  if (value instanceof String) {
+    return true;
+  }
+  return false;
 }
 
 /**
